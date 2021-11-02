445b4b79b00bd07211e9409aba1b694ec87d621c
HariharanT99 <hariharant99@gmail.com>
2021-11-02 20:35:58 +0530

    Un wanted 1

--- src/app/modules/hr/hr-routing.module.ts
+++ src/app/modules/hr/hr-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HrAddApplicantComponent } from './page/hr-add-applicant/hr-add-applicant.component';
 import { HrDashboardComponent } from './page/hr-dashboard/hr-dashboard.component';
 
 const routes: Routes = [
@@ -7,6 +8,10 @@ const routes: Routes = [
     path: '',
     component: HrDashboardComponent,
   },
+  {
+    path:'add-applicant',
+    component: HrAddApplicantComponent
+  }
 ];
 
 @NgModule({
